d2e835c308e1d824d7f02433c6ec1322260e9c2b
02-AthenaToh <scorch_fire@hotmail.com>
2022-10-31 16:57:04 +0800

    Addition updated

---
+++ Lab10 - Project Starter/Assets/Tests/TestSuite.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestSuite
+    {
+        private float result = 0.0f;
+
+        [Test]
+        public void TestAddition()
+        {
+            // Use the Assert class to test conditions
+            result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnityAddition()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);
+        }
+
+        [Test]
+        public void TestSubtraction()
+        {
+            // Use the Assert class to test conditions
+            result = Calculator.CalculatePair(5, 2, "-");
+            Assert.AreEqual(result, 3);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnitySubtraction()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "-");
+            Assert.AreEqual(result, 3);
+        }
+
+        [Test]
+        public void TestMultiplication()
+        {
+            // Use the Assert class to test conditions
+            result = Calculator.CalculatePair(5, 2, "*");
+            Assert.AreEqual(result, 10);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnityMultiplication()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "*");
+            Assert.AreEqual(result, 10);
+        }
+
+        [Test]
+        public void TestDivision()
+        {
+            // Use the Assert class to test conditions
+            result = Calculator.CalculatePair(4, 2, "/");
+            Assert.AreEqual(result, 2);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnityDivision()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+
+            result = Calculator.CalculatePair(4, 2, "/");
+            Assert.AreEqual(result, 2);
+        }
+    }
+}
